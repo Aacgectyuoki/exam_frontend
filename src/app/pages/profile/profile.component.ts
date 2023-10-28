3a7eb1a5f91fb2f5bab42c9589560d6935347c8b
Shahbaz Khan <shahbazkhaniq@gmail.com>
2023-10-29 04:58:29 +0500

    implemented profile update

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { LoginService } from 'src/app/services/login.service';
+import { UpdateProfileComponent } from 'src/app/pages/update profile/update-profile/update-profile.component';
 
 @Component({
   selector: 'app-profile',
@@ -8,7 +10,7 @@ import { LoginService } from 'src/app/services/login.service';
 })
 export class ProfileComponent implements OnInit {
   user: any = {};
-  constructor(private login: LoginService) {}
+  constructor(private login: LoginService, private dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.user = this.login.getUser();
@@ -20,5 +22,19 @@ export class ProfileComponent implements OnInit {
     //     alert('error');
     //   }
     // );
+  }
+
+  
+  openUpdateProfileDialog() {
+    const dialogRef = this.dialog.open(UpdateProfileComponent, {
+      data: this.user // Pass the user data to the update profile dialog
+    });
+
+    dialogRef.afterClosed().subscribe((updatedUser) => {
+      if (updatedUser) {
+        // Handle the updated user data here
+        this.user = updatedUser;
+      }
+    });
   }
 }
